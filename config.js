b8331b37ffd2940602a7dd8a885ab9e7cd218927
Tanner Mickelson <tanner@icicle-games.com>
2018-03-18 20:49:10 +0900

    Config tweaks

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   port: parseInt(process.env.PORT || 8080),
-  width: parseInt(process.env.WIDTH || 960),
-  height: parseInt(process.env.HEIGHT || 540),
-  fps: parseInt(process.env.FPS || 12)
+  width: parseInt(process.env.WIDTH || 640),
+  height: parseInt(process.env.HEIGHT || 480),
+  fps: parseInt(process.env.FPS || 24)
 };
