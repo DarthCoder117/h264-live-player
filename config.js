bee428ea562003347793353092cbdd071cedb9c4
Tanner Mickelson <tanner@icicle-games.com>
2018-03-21 20:54:27 +0900

    Change background to black. Go back to 24 FPS

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
   port: parseInt(process.env.PORT || 8080),
   width: parseInt(process.env.WIDTH || 768),
   height: parseInt(process.env.HEIGHT || 432),
-  fps: parseInt(process.env.FPS || 30)
+  fps: parseInt(process.env.FPS || 24)
 };
